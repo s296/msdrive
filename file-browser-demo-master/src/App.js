1d5cc0a3c9d1b142f24028c852b6b9a67461305b
garg <garg@mail.com>
2020-06-20 13:50:33 +0530

    initial commit

---
+++ file-browser-demo-master/src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Text } from 'office-ui-fabric-react';
+import { GraphFileBrowser } from '@microsoft/file-browser';
+import { UserAgentApplication } from 'msal';
+import {config} from './config';
+
+const CLIENT_ID = config.CLIENT_ID;
+const SCOPES = ['files.readwrite.all', 'user.read'];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: null
+    };
+
+    this.msal = new UserAgentApplication(CLIENT_ID);
+  }
+
+  componentDidMount() {
+    this._tryGetMsalToken();
+  }
+
+  render() {
+    const { token } = this.state;
+
+    return (
+      <div className="App">
+        <Text variant="xxLarge">File Browser Demo</Text>
+        {
+          token ?
+            <GraphFileBrowser
+              getAuthenticationToken={ () => Promise.resolve(token) }
+              onSuccess={ alert }
+              onRenderCancelButton={ () => null }
+              onRenderSuccessButton={ () => null }
+            />
+            :
+            <Text block onClick={this._getMsalToken}>[log in]</Text>
+        }
+      </div>
+    );
+  }
+
+  _acquireAccessToken = () => {
+    this.msal.acquireTokenSilent(SCOPES).then(token => {
+      this.setState({ token });
+    }, err => {
+      this.msal.acquireTokenRedirect(SCOPES);
+    });
+  }
+
+  _loginPromptAndAuthenticate = () => {
+    this.msal.loginPopup(SCOPES).then(idToken => {
+      this._acquireAccessToken()
+    });
+  }
+
+  _tryGetMsalToken = () => {
+    const user = this.msal.getUser();
+
+    if (user) {
+      this._acquireAccessToken();
+    }
+  }
+
+  _getMsalToken = () => {
+    const user = this.msal.getUser();
+
+    if (user) {
+      this._acquireAccessToken();
+    } else {
+      this._loginPromptAndAuthenticate();
+    }
+  }
+}
+
+export default App;
